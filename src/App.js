b3a772cec6d45d2a46a012ae208ee089b9597635
qzmp10 <loganrobitaille@outlook.com>
2022-10-23 12:16:31 -0400

    creating componenets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import CardContainer from './components/cardContainer';
 
 function App() {
   return (
-    <div>Hello!</div>
+    <>
+      <h1 className='header'>JOJO'S BIZARRE MEMORY CARD GAME</h1>
+      <div className=''></div>
+      <CardContainer />
+    </>
   );
 }
 
---
+++ src/components/cardContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CardContainer(props) {
+    return (
+        <div className='cardContainer'>CardContainer
+        </div>
+    )
+}
